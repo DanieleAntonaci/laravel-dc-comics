41e22ea3689d4be6dc7049dd3eeab280a7404ad4
DanieleAntonaci <danieleanto96@gmail.com>
2023-02-06 18:51:37 +0100

    Add css

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,17 +1,19 @@
 @extends('layout.main-layout')
 
 @section('list-person')
-<a href="{{route('person.create')}}">Create new perosn</a>
+<a class="new-person" href="{{route('person.create')}}">Create new perosn</a>
     <ul>
         @foreach ($people as $person)
         <li>
             <a href="{{route('person.show', $person)}}">
                 {{$person -> name}} {{$person -> lastName}}
             </a>
-            -
-            <a href="{{route('person.destroy', $person)}}">Delete</a>
-            -
-            <a href="{{route('person.edit',$person)}}">Update</a>
+            <div>
+
+                <a class="delete-person" href="{{route('person.destroy', $person)}}">Delete</a>
+                -
+                <a class="update-person" href="{{route('person.edit',$person)}}">Update</a>
+            </div>
         </li>
         @endforeach
     </ul>
